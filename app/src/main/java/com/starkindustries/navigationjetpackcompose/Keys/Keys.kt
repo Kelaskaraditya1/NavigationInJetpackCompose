f03fd198b61113fda8202d76f032bc12fdb9dd25
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2025-05-22 12:09:33 +0530

    Preparing for migration

--- app/src/main/java/com/starkindustries/navigationjetpackcompose/Keys/Keys.kt
+++ app/src/main/java/com/starkindustries/navigationjetpackcompose/Keys/Keys.kt
@@ -4,5 +4,6 @@ class Keys {
     companion object{
         const val MAIN_SCREEN_ROUTE:String="MAIN_SCREEN_ROUTE"
         const val SECOND_SCREEN_ROUTE:String="SECOND_SCREEN_ROUTE"
+        const val
     }
 }
